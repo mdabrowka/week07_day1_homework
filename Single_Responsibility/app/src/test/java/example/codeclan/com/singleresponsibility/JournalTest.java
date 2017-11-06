04a33012a7679eb434c57356321d6ca1c6602319
Marta <martadabrowka@gmail.com>
2017-11-06 20:33:49 +0000

    added journal class

---
+++ Single_Responsibility/app/src/test/java/example/codeclan/com/singleresponsibility/JournalTest.java
@@ -0,0 +1,33 @@
+package example.codeclan.com.singleresponsibility;
+
+import org.junit.Test;
+import org.junit.Before;
+import static org.junit.Assert.assertEquals;
+
+
+/**
+ * Created by marta on 06/11/2017.
+ */
+
+public class JournalTest {
+    Journal journal;
+    Bear bear;
+    Salmon food;
+
+    @Before
+    public void setup() {
+        journal = new Journal();
+        bear = new Bear();
+        food = new Salmon();
+    }
+
+    @Test
+    public void canWriteJournalEntry(){
+        bear.eat(food);
+        bear.writeJournal("Dear Diary, I ate " + bear.foodCount() + " salon today.");
+        assertEquals("Dear Diary, I ate 1 salon today.", bear.getLastJournalEntry());
+    }
+
+
+
+}
