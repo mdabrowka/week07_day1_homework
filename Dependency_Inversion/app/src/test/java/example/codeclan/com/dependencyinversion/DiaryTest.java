ab91b9f3076f9b8001c1982e0e5c9c97f6dec5a1
Marta <martadabrowka@gmail.com>
2017-11-06 21:29:24 +0000

    added Climbable interface and created Iceberg class to implement it

---
+++ Dependency_Inversion/app/src/test/java/example/codeclan/com/dependencyinversion/DiaryTest.java
@@ -0,0 +1,8 @@
+package example.codeclan.com.dependencyinversion;
+
+/**
+ * Created by marta on 06/11/2017.
+ */
+
+public class DiaryTest {
+}
---
+++ Interface_Segregation/app/src/main/java/example/codeclan/com/interfacesegregation/Climbable.java
@@ -0,0 +1,8 @@
+package example.codeclan.com.interfacesegregation;
+
+/**
+ * Created by marta on 06/11/2017.
+ */
+
+public interface Climbable {
+}
